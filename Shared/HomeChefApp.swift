474b22afa013a4b919bbe8600de71d82e6b867b4
omidamin6 <omidamin6@gmail.com>
2021-06-28 16:21:49 -0400

    Initial Commit

---
+++ Shared/HomeChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeChefApp.swift
+//  Shared
+//
+//  Created by omid amin on 6/28/21.
+//
+
+import SwiftUI
+
+@main
+struct HomeChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
